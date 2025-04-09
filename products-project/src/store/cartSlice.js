e60245ab6c7e4b207eefe5d0ed75c380265376a6
unknown <aleksandrekasrashvili@gmail.com>
2025-04-10 00:44:10 +0400

    added new slice to the store for items in cart

---
+++ products-project/src/store/cartSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const cartSlice = createSlice({
+    name:'cartSlice',
+    initialState:{
+        cartItems: localStorage.getItem('cartItem') ? JSON.parse(localStorage.getItem('cartItem')) : []
+    },
+    reducers:{
+        addItem:(state,action)=>{
+            state.cartItems.push(action.payload)
+        }
+    }
+
+})
+
+
+export const {addItem} = cartSlice.actions
+export default cartSlice.reducer
--- products-project/src/store/store.js
+++ products-project/src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemSlice from './itemSlice'
-
+import cartSlice from './cartSlice'
 
 
 export const store = configureStore({
     reducer:{
-        id:itemSlice       
+        id:itemSlice,
+        cartItem:cartSlice       
     }
 })
